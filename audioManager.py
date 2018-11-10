11f4d59737a2d83a702acefe4b2e11b198a9cca0
mcquiddy <cristian95mc@gmail.com>
2018-11-10 03:07:29 -0600

    se añadio la funcion de normalizar la energia de los audios

--- audioManager.py
+++ audioManager.py
@@ -6,12 +6,21 @@ import matplotlib.cm as cmap
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import NavigationToolbar2
 from playsound import playsound
+from pylab import*
+import math
+
+
 
 home = NavigationToolbar2.home
 
 txtNumbers = ['cero','uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve','diez','once','doce','trece','catorce','quince']
 global currentNumber
+global currentID
+global audioAverage
+audioAverage = 0
+
 currentNumber = 0
+currentID = 26
 
 '''
 *   Returns a string that identifies the file that is required
@@ -42,29 +51,70 @@ def prevNumber():
 
 def nextUser(self, *args, **kwargs):
     nextNumber()
-    plotSTFT(getFileName(currentNumber,26,1),txtNumbers[currentNumber])
+    plotSTFT(getFileName(currentNumber,currentID,1),txtNumbers[currentNumber])
 
     home(self, *args, **kwargs)
 
 def prevUser(self, *args, **kwargs):
     prevNumber()
-    plotSTFT(getFileName(currentNumber,26,1),txtNumbers[currentNumber])
+    plotSTFT(getFileName(currentNumber,currentID,1),txtNumbers[currentNumber])
 
     home(self, *args, **kwargs)
 
 NavigationToolbar2.forward = nextUser
 NavigationToolbar2.back = prevUser
 
+
+
+'''
+* Returns average energy of all recorded
+'''
+def normalizeSignalEnergy():
+    audio = 0 # energy sum of each audio
+    count=0 # count numbers
+    for num in range(0, 16): # numero 
+        for idx in range(0, 31): # student id 
+            for contx in range(1, 4): # context
+                try:
+                    sample_rateTemp,samplesTemp = wav.read(getFileName(num,idx,contx))
+                    # Ecuation of energy, means take the average of each sample 
+                    rms_val = sqrt(mean(samplesTemp**2))
+                    if (math.isnan(rms_val) != True):
+                        audio = audio + rms_val
+                    count =count +1       
+                except :
+                   print("Error al abrir",getFileName(num,idx,contx))
+                   # if you get here it means an error happende, maybe you should warn the user
+                   # but doing pass will silently ignore it
+                   pass
+    
+    
+    print("Audios leidos")
+    print(count) 
+    audioAverageTemp=audio/count #
+    print("Energia promedio")
+    print(audioAverageTemp)
+    return audioAverageTemp
+
+
 #--------------------------------------------------------------------------------------
 
 def plotSTFT(pFilename,pNumberText):
     sample_rate, samples = wav.read(pFilename)
-    playsound(pFilename)
-
+    print("Numero de muestra")
     print(pNumberText)
-
+    print("original")
+    playsound(pFilename)
+    
+    # Test audio normalize
+    wav.write("test.wav", sample_rate, samples /audioAverage)
+    print("normalizado")
+    playsound("test.wav")
+    
+    # plot of frencuence vs time
     f, t, Zxx = signal.stft(samples, fs=sample_rate)
     plt.pcolormesh(t, f, np.abs(Zxx), cmap='Reds')
     plt.show()
 
-plotSTFT(getFileName(currentNumber,26,1),txtNumbers[currentNumber])
+audioAverage = normalizeSignalEnergy() # Energia promedio de los audios
+plotSTFT(getFileName(currentNumber,currentID,1),txtNumbers[currentNumber])
